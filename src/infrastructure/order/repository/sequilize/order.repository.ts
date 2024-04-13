8241c0b225fc4b9c0a2273136e0fa5428b2bbb88
Thales Eduardo <thalesdev22@gmail.com>
2024-04-13 17:57:22 -0300

    main - Adicionando previa de Order.repository

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,9 @@
 import { Order } from "../../../../domain/checkout/entities/order";
+import { OrderRepositoryInterface } from "../../../../domain/checkout/repository/order-repository.interface";
 import { OrderItemModel } from "./order-item.model";
 import { OrderModel } from "./order.model";
 
-export class OrderRepository {
+export class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -21,5 +22,35 @@ export class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  //impleementar para desafio e nos testes tmb
+  async update(entity: Order): Promise<void> {
+    // await OrderModel.update(
+    //   {
+    //     name: entity.name,
+    //     price: entity.price,
+    //   },
+    //   {
+    //     where: {
+    //       id: entity.id,
+    //     },
+    //   }
+    // );
+  }
+
+  async find(id: string): Promise<Order> {
+    const productModel: Order | any = await OrderModel.findOne({
+      where: { id },
+    });
+    return new Order(productModel.id, productModel.name, productModel.price);
+  }
+
+  async findAll(): Promise<Order[]> {
+    const productModels: any[] = await OrderModel.findAll();
+    return productModels;
+    // .map
+    // (productModel) =>
+    //   new Order(productModel.id, productModel.name, productModel.price)
   }
 }
