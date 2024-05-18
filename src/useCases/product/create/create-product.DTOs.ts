e35affa5eff1cdd3790724631deabe546e1fe7ee
Thales Eduardo <thalesdev22@gmail.com>
2024-05-18 14:11:26 -0300

    main - Adicionando teste de integration no usecase de product

---
+++ src/useCases/product/create/create-product.DTOs.ts
@@ -0,0 +1,11 @@
+export interface InputCreateProductDTOs {
+  type: string;
+  name: string;
+  price: number;
+}
+
+export interface OutputCreateProductDTOs {
+  id: string;
+  name: string;
+  price: number;
+}
---
+++ src/useCases/product/create/create-product.usecase.ts
@@ -0,0 +1,32 @@
+import { Product } from "../../../domain/product/entities/product";
+import { ProductFactory } from "../../../domain/product/factory/product.factory";
+import { ProductRepositoryInterface } from "../../../domain/product/repository/product-repository.interface";
+import {
+  InputCreateProductDTOs,
+  OutputCreateProductDTOs,
+} from "./create-product.DTOs";
+
+export class CreateProductUseCase {
+  private productRepositoryInterface: ProductRepositoryInterface;
+
+  constructor(customerRepository: ProductRepositoryInterface) {
+    this.productRepositoryInterface = customerRepository;
+  }
+
+  async execute(
+    input: InputCreateProductDTOs
+  ): Promise<OutputCreateProductDTOs> {
+    const res = ProductFactory.create(input.type, input.name, input.price);
+    console.log("res", res);
+
+    const product = new Product(res.id, res.name, res.price);
+
+    await this.productRepositoryInterface.create(product);
+
+    return {
+      id: res.id,
+      name: res.name,
+      price: res.price,
+    };
+  }
+}
